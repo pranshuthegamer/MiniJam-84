a5f2b2c008e32ee8644441fea1943d5d18555964
12345qwertyman12345@gmail.com <12345qwertyman12345@gmail.com>
2021-07-09 16:43:10 +0530

    Auto.gd is now a singleton Changed window settings

--- Scripts/Auto.gd
+++ Scripts/Auto.gd
@@ -1,7 +1,11 @@
 extends Node
 
+var MainScene = null
+
 func _ready():
 	if get_node_or_null("/root/Main") != null:
-		pass
+		MainScene = get_node("/root/Main")
+		var tempscene = preload("res://Scenes/MainMenu.tscn").instance()
+		MainScene.get_node("GUI").add_child(tempscene)
 	else:
 		pass
