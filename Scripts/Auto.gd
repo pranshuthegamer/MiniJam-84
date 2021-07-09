e127c4eb41c394d265ae3deaca683100f0dfc5ef
= <12345qwertyman12345@gmail.com>
2021-07-09 16:51:52 +0530

    pixelated main menu

--- Scripts/Auto.gd
+++ Scripts/Auto.gd
@@ -4,6 +4,7 @@ var MainScene = null
 
 func _ready():
 	if get_node_or_null("/root/Main") != null:
+		pass
 		MainScene = get_node("/root/Main")
 		var tempscene = preload("res://Scenes/MainMenu.tscn").instance()
 		MainScene.get_node("GUI").add_child(tempscene)
